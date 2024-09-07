d4a9d9ba1c331791415f694c8546bf7ccd1ce97e
flareseek <flareseek@gmail.com>
2024-09-08 01:52:47 +0900

    chore: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ function App() {
   return (
     <div>
       <h1>Hello, KNU Festival!</h1>
+      {/*
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        */}
     </div>
   );
 }
