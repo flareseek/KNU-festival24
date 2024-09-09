de84617ace15f1e6567a665fd75effd237995845
Jeje <jjh4450jjh4450@gmail.com>
2024-09-10 03:53:59 +0900

    test: 로고 이미지 빌드 테스트 14kb -> 7kb 압축 효과가 나타남, 80프로 손실 압축을 적용 했음에도, 눈에 띄는 화질 저하가 없었음

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,10 @@
+import logo from '../../assets/logo.png';
+
 export default function Main() {
   return (
     <div>
       <h1>Main</h1>
+        <img src={logo} alt="logo" />
     </div>
   );
 }
