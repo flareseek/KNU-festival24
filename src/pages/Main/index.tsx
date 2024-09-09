90f19e8bca1d14ff37e21508da1e72fb0ee559e0
Jeje <jjh4450jjh4450@gmail.com>
2024-09-10 04:26:50 +0900

    fix: prettier check

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,10 +1,10 @@
-import logo from '../../assets/logo.png';
+import logo from "../../assets/logo.png";
 
 export default function Main() {
   return (
     <div>
       <h1>Main</h1>
-        <img src={logo} alt="logo" />
+      <img src={logo} alt="logo" />
     </div>
   );
 }
