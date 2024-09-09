05e7904d384bf1cf10a0f1e339be13a079f5034d
Jeje <jjh4450jjh4450@gmail.com>
2024-09-10 03:41:18 +0900

    chore: 빌드시 webp 자동 변환 기능 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { imagetools } from 'vite-imagetools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,12 @@ export default defineConfig({
         dimensions: false, // svg의 width, height 속성을 제거하고 css에 의존이 되도록 함
       }
     }),
+    imagetools({
+      defaultDirectives: () => new URLSearchParams({
+        format: 'webp',
+        quality: '80',   // 품질을 80%로 설정하여 손실 압축 적용
+        removeMetadata: ''
+      })
+    })
   ],
 });
