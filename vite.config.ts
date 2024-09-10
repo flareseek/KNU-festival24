8308996c976868b8ff420345ff01ccd4666c6f63
Jeje <jjh4450jjh4450@gmail.com>
2024-09-10 14:11:37 +0900

    feat(vite.cofing): vite 번들링시 압축이 가능하게 구성함

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import { imagetools } from "vite-imagetools";
+import compression from 'vite-plugin-compression2';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     vanillaExtractPlugin(),
+    compression({
+      include: /\.(js|css|html|svg)$/, // 압축할 파일 확장자 지정
+      threshold: 1400, // 1.4kb 이상 파일만 압축
+    }),
     svgr({
       svgrOptions: {
         dimensions: false, // svg의 width, height 속성을 제거하고 css에 의존이 되도록 함
