288994ed59588f9c44539993470e96313ad90c6e
flareseek <flareseek@gmail.com>
2024-09-10 15:48:13 +0900

    chore: fix prettier ignore

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import { imagetools } from "vite-imagetools";
-import compression from 'vite-plugin-compression2';
+import compression from "vite-plugin-compression2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
