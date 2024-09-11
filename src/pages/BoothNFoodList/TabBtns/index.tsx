1e003cd250bcf55588c79ba3b25e802687e0d64d
dandamdandam <glue0440@gmail.com>
2024-09-11 23:37:36 +0900

    chore: 탭 버튼 훅 주석

--- src/pages/BoothNFoodList/TabBtns/index.tsx
+++ src/pages/BoothNFoodList/TabBtns/index.tsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
 
+// TODO: 타임테이블에서 가져다 써도 될 것 같아요
+/**
+ * 탭 버튼 컴포넌트를 사용하기 위한 커스텀 훅
+ * 활용 예시: const { activeTab, TabBtns } = useTabBtns(["탭1", "탭2", "탭3"]);
+ * @param tabNames 탭 버튼 상태 배열
+ * @returns activeTab: 현재 선택된 탭 상태, TabBtns: 탭 버튼 컴포넌트
+ */
 export default function useTabBtns(tabNames: string[]) {
   const [activeTab, setActiveTab] = useState(tabNames[0]);
   const TabBtns = ({ tabBtnNames }: { tabBtnNames?: string[] }) => {
