7b6cc4b85c309c05935f9c350400193d920c4563
dandamdandam <glue0440@gmail.com>
2024-09-13 11:53:51 +0900

    feat: reset css 수정

--- src/shared/styles/globalStyle.css.ts
+++ src/shared/styles/globalStyle.css.ts
@@ -5,15 +5,9 @@ import { vars } from "./vars.css";
 
 globalStyle("body", {
   position: "relative",
-  margin: 0,
-  padding: 0,
   backgroundImage: `url(${backgroundImg})`, // 배경 이미지 경로
   backgroundSize: "100% auto",
   fontFamily: vars.font.pretendardRegular,
-});
-
-globalStyle("ol, ul", {
-  listStyle: "none",
 });
 
 globalStyle("button", {
@@ -27,70 +21,10 @@ globalStyle("button", {
 });
 
 globalStyle("input, textarea, select", {
-  background: "var(--side-c)",
   color: "inherit",
   border: "none",
-  padding: "5px",
   font: "inherit",
   cursor: "pointer",
   outline: "inherit",
   resize: "none",
-  colorScheme: "dark",
-  borderRadius: "5px",
 });
-
-globalStyle(
-  `html,
-  body,
-  var,
-  b,
-  u,
-  i,
-  center,
-  dl,
-  dt,
-  dd,
-  ol,
-  ul,
-  li,
-  fieldset,
-  form,
-  label,
-  legend,
-  table,
-  caption,
-  tbody,
-  tfoot,
-  thead,
-  tr,
-  th,
-  td,
-  article,
-  aside,
-  canvas,
-  details,
-  embed,
-  figure,
-  figcaption,
-  footer,
-  header,
-  hgroup,
-  menu,
-  nav,
-  output,
-  ruby,
-  section,
-  summary,
-  time,
-  mark,
-  audio,
-  video`,
-  {
-    margin: 0,
-    padding: 0,
-    border: 0,
-    fontSize: "100%",
-    font: "inherit",
-    verticalAlign: "baseline",
-  },
-);
