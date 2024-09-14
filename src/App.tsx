0459d1316bc214bcaa8579409e5f4a49e6ee39fd
Jeje <jjh4450jjh4450@gmail.com>
2024-09-14 16:26:10 +0900

    feat(Router): 라우터 객체 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import RouterInfo from "./RouterInfo.tsx";
 
-import Layout from "./components/Layout/index.tsx";
-import Main from "./pages/Main/index.tsx";
-import Artist from "./pages/Artist/index.tsx";
-import Booth from "./pages/Booth/index.tsx";
-import BoothNFoodList from "./pages/BoothNFoodList/index.tsx";
-import Foodtruck from "./pages/Foodtruck/index.tsx";
-import Makers from "./pages/Makers/index.tsx";
-import Map from "./pages/Map/index.tsx";
-import Notice from "./pages/Notice/index.tsx";
-import QnA from "./pages/QnA/index.tsx";
-import Timetable from "./pages/Timetable/index.tsx";
 
-const RouterPath = [
-  {
-    path: "",
-    element: <Main />,
-  },
-  {
-    path: "artist/:id",
-    element: <Artist />,
-  },
-  {
-    path: "booth/:id",
-    element: <Booth />,
-  },
-  {
-    path: "booth_foodtruck_list",
-    element: <BoothNFoodList />,
-  },
-  {
-    path: "foodtruck/:id",
-    element: <Foodtruck />,
-  },
-  {
-    path: "makers",
-    element: <Makers />,
-  },
-  {
-    path: "map",
-    element: <Map />,
-  },
-  {
-    path: "notice",
-    element: <Notice />,
-  },
-  {
-    path: "QnA",
-    element: <QnA />,
-  },
-  {
-    path: "timetable",
-    element: <Timetable />,
-  },
-];
+const RouterPath = RouterInfo.map((info) => {
+    return {
+        path: info.path,
+        element: info.element,
+    }
+});
+
 const router = createBrowserRouter([
   {
     path: "/",
