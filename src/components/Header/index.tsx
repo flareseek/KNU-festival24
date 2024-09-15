213727f23129a996c96681b10046585d05bda701
jjh4450 <jjh4450git@gmail.com>
2024-09-15 22:53:04 +0900

    docs(Header): 주석 작성

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -17,15 +17,33 @@ import {
 import { RouterInfoType } from "../../shared/types/Route";
 import Lottie, { LottieRefCurrentProps } from "lottie-react";
 
+/**
+ * 현재 경로인지 확인하는 함수
+ * @param path 비교할 경로
+ * @param pathname 현재 경로(useLocation().pathname)
+ * @return boolean
+ */
 const isCurrentPath = (path: string, pathname: string): boolean =>
   !!matchPath({ path, end: true }, pathname);
 
+/**
+ * Header 컴포넌트
+ * @component
+ * @returns {React.ReactElement} Header 컴포넌트 요소
+ */
 export const Header: React.FC = () => {
   const [isActive, setIsActive] = useState(false);
   const [currentPage, setCurrentPage] = useState("/");
   const location = useLocation();
+
+  // lottie 애니메이션을 제어하기 위한 ref | 참고: https://lottiereact.com/#calling-the-methods
   const lottieRef = useRef<LottieRefCurrentProps>(null);
 
+  /**
+   * 토글 버튼 클릭 이벤트 핸들러
+   * @function handleToggle
+   * @return {void}
+   */
   const handleToggle = useCallback(() => {
     setIsActive((prevState) => {
       const newState = !prevState;
@@ -40,6 +58,9 @@ export const Header: React.FC = () => {
     });
   }, []);
 
+  /**
+   * 컴포넌트가 마운트될 때 현재 페이지를 설정하고, isActive를 false로 초기화함
+   */
   useEffect(() => {
     const currentRoute = RouterInfo.find((route) =>
       isCurrentPath(route.path, location.pathname)
@@ -51,10 +72,15 @@ export const Header: React.FC = () => {
 
   return (
     <header className={headerStyles}>
+      {/*로고 부분*/}
       <Link to="/">
         <Logo className={logoStyles} />
       </Link>
+
+      {/*현재 페이지 출력 부분*/}
       <span className={currentPageStyles}>{currentPage}</span>
+
+      {/*토글 버튼 (lottie)*/}
       <Lottie
         animationData={Menu}
         className={toggleBtnStyles}
@@ -63,6 +89,8 @@ export const Header: React.FC = () => {
         autoplay={false}
         loop={false}
       />
+
+      {/*페이지 메뉴*/}
       <nav className={`${menuStyles} ${isActive ? "active" : ""}`}>
         <ul className={menuListStyles}>
           {RouterInfo.filter((item) => item.expose)
