ec9335d539596bf09644adbeb8aa527df8f02aed
jjh4450 <jjh4450git@gmail.com>
2024-09-17 21:41:15 +0900

    refactor(routerUtils): header import 과정에서 router utill 추가 - isCurrentPath: 현재 경로인지 확인하는 함수 - useCurrentPage: 현재 페이지 객체를 반환하는 커스텀 훅

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { Link, matchPath, useLocation } from "react-router-dom";
+import React, { useCallback, useEffect, useRef } from "react";
+import { Link, useLocation } from "react-router-dom";
 import routerInfo from "../../shared/routing/routerInfo";
 import Logo from "../../assets/logo.svg?react";
 import Menu from "../../assets/menu_buton.json";
@@ -16,82 +16,40 @@ import {
 } from "./.css.ts";
 import { routerInfoType } from "../../shared/types/routing.ts";
 import Lottie, { LottieRefCurrentProps } from "lottie-light-react";
+import { useMediaQuery, useToggle } from "./customHooks.tsx";
 
-/**
- * 현재 경로인지 확인하는 함수
- * @param path 비교할 경로
- * @param pathname 현재 경로(useLocation().pathname)
- * @return boolean
- */
-const isCurrentPath = (path: string, pathname: string): boolean =>
-  !!matchPath({ path, end: true }, pathname);
-
+import { isCurrentPath, useCurrentPage } from "../../shared/routing/routerUtils.ts";
 
 /**
  * Header 컴포넌트
  * @component
  * @returns {React.ReactElement} Header 컴포넌트 요소
  */
-export const Header: React.FC = () => {
-  const [isActive, setIsActive] = useState(false);
-  const [currentPage, setCurrentPage] = useState("/");
+const Header: React.FC = () => {
+  const [isActive, toggleActive] = useToggle(false);
+  const currentPage = useCurrentPage();
   const location = useLocation();
-  const [width, setWidth] = useState(window.innerWidth);
-
-  const handleResize = () => {
-    setWidth(window.innerWidth);
-  };
-
-  // if lodash is available, plz use this - do not remove -
-  // const handleResize = debounce(() => {
-  //   setWidth(window.innerWidth);
-  // }, 200);
-
-  // lottie 애니메이션을 제어하기 위한 ref | 참고: https://lottiereact.com/#calling-the-methods
   const lottieRef = useRef<LottieRefCurrentProps>(null);
+  const isDesktop = useMediaQuery('(min-width: 1000px)');
 
-  /**
-   * 토글 버튼 클릭 이벤트 핸들러
-   * @function handleToggle
-   * @return {void}
-   */
   const handleToggle = useCallback(() => {
-    setIsActive((prevState) => {
-      const newState = !prevState;
-      if (lottieRef.current) {
-        if (newState) {
-          lottieRef.current.playSegments([0, 60], true);
-        } else {
-          lottieRef.current.playSegments([60, 120], true);
-        }
-      }
-      return newState;
-    });
-  }, []);
-
-  /**
-   * 컴포넌트가 마운트될 때 현재 페이지를 설정하고, isActive를 false로 초기화함
-   */
-  useEffect(() => {
-    const currentRoute = routerInfo.find((route) => isCurrentPath(route.path, location.pathname));
-    setCurrentPage(currentRoute?.korean ?? "/");
-    setIsActive(false);
-    lottieRef.current?.setSpeed(1.5);
-  }, [location.pathname]);
-
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    return () => {
-      // cleanup
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (width >= 1000 && isActive) {
-        handleToggle();
+    toggleActive();
+    if (lottieRef.current) {
+      lottieRef.current.playSegments(isActive ? [60, 120] : [0, 60], true);
     }
-  }, [width]);
+  }, [isActive, toggleActive]);
+
+  useEffect(() => {
+    if (lottieRef.current) {
+      lottieRef.current.setSpeed(1.5);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isDesktop && isActive) {
+      handleToggle();
+    }
+  }, [isDesktop, isActive, handleToggle]);
 
   return (
     <header className={headerStyles}>
@@ -101,7 +59,7 @@ export const Header: React.FC = () => {
       </Link>
 
       {/*현재 페이지 출력 부분*/}
-      <span className={currentPageStyles}>{currentPage}</span>
+      <span className={currentPageStyles}>{currentPage.korean}</span>
 
       {/*토글 버튼 (lottie)*/}
       <Lottie
@@ -117,14 +75,14 @@ export const Header: React.FC = () => {
       <nav className={`${menuStyles} ${isActive ? "active" : ""}`}>
         <ul className={menuListStyles}>
           {routerInfo
-            .filter((item) => item.expose)
+            .filter((item: routerInfoType) => item.expose)
             .sort((a: routerInfoType, b: routerInfoType) => a.korean.localeCompare(b.korean))
-            .map((item) => (
+            .map((item: routerInfoType) => (
               <li key={item.path} className={menuItemStyles}>
                 <Link
                   to={item.path}
                   className={`${menuItemLinkStyles} ${
-                    isCurrentPath(item.path, location.pathname) ? highlightStyles : ""
+                    isCurrentPath(item, location) ? highlightStyles : ""
                   }`}
                   onClick={handleToggle}
                 >
@@ -138,4 +96,4 @@ export const Header: React.FC = () => {
   );
 };
 
-export default Header;
+export default React.memo(Header);
--- src/shared/types/routing.ts
+++ src/shared/types/routing.ts
@@ -1,6 +1,15 @@
 // 라우팅 정보 객체 타입
 import React, { ComponentType } from "react";
 
+/**
+ * 라우팅 정보 객체 타입
+ * @interface
+ * @property {string} path - 라우팅 경로
+ * @property {React.LazyExoticComponent<ComponentType<unknown>>} element - 라우팅 컴포넌트
+ * @property {string} english - 영어 이름
+ * @property {string} korean - 한글 이름
+ * @property {boolean} expose - 헤더 노출 여부
+ */
 export interface routerInfoType {
   path: string;
   element: React.LazyExoticComponent<ComponentType<unknown>>;
