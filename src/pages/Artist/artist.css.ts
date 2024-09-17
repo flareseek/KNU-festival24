42490dcc9578c53c12449b863c7f462e5c87104c
Kim-jaeyeon <jy020507@naver.com>
2024-09-18 00:11:50 +0900

    Feat(artist 뼈대 완성)
    아티스트 주요 인기곡에서 스크롤이 끝나도록 설정/ 아티스트 사진이 정사각형으로 유지되도록 설정

--- src/pages/Artist/artist.css.ts
+++ src/pages/Artist/artist.css.ts
@@ -1,75 +1,33 @@
-import { style, globalStyle, globalFontFace } from "@vanilla-extract/css";
-
-// body에 글로벌 스타일 적용
-globalStyle("body", {
-  position: "relative",
-  margin: 0,
-  padding: 0,
-  backgroundImage: 'url("/src/assets/background.svg")', // 배경 이미지 경로
-  backgroundSize: "cover", // 배경 이미지를 화면 전체에 맞춤
-  backgroundPosition: "center", // 이미지를 화면 중앙에 위치
-  backgroundRepeat: "no-repeat", // 이미지를 반복하지 않도록 설정
-  width: "100%",
-  height: "100vh", // 전체 화면 높이 적용
-  overflow: "hidden", // 스크롤바가 나타나지 않도록 설정
-  fontFamily: `'Pretendard', sans-serif`,
-});
-globalStyle("#root", {
-    height:"100%"
-  });
-  globalStyle("#root>div", {
-    height:"100%"
-  });
-  globalStyle("#root>div>div", {
-    height:"100%"
-  });
-globalStyle("::-webkit-scrollbar", {
-  width: "0px",
-  height: "0px",
-});
-
-globalFontFace("PyeongChangPeace", {
-  src: 'url("/src/assets/PyeongChangPeace-Bold.otf") format("opentype")',
-  fontWeight: "bold",
-  fontStyle: "normal",
-});
-
-
-export const titleText = style({
-  fontSize: 25,
-  fontWeight: 500,
-  color: "white",
-  marginTop: 50,
-});
-
-// 상단의 빨간 컨테이너
-export const container = style({
-  width: "100%",
-  height: "10%",
-  display: "flex",
-  justifyContent: "center",
-});
+import { style } from "@vanilla-extract/css";
+import { vars } from "../../shared/styles/vars.css";
 
 // 중간에 검색창을 배치하는 컨테이너
 export const mid = style({
   position: "relative",
   width: "100%",
-  height:"90%",
+  height: "100%",
   display: "flex",
-  margin: "20px 0",
   alignItems: "center",
   flexDirection: "column",
+  margin:"0 0 20px 0",
 });
 
 export const artistPicContainer = style({
   width: "100%",
-  height:'50%',
-  backgroundImage: 'url("/src/assets/day6.svg")',
+  height: "100vw",
+  backgroundColor: "gray",
   backgroundSize: "cover", // 배경 이미지를 화면 전체에 맞춤
   backgroundPosition: "center", // 이미지를 화면 중앙에 위치
   backgroundRepeat: "no-repeat", // 이미지를 반복하지 않도록 설정
 });
 
+export const artistDetailContainer = style({
+  width: "100%",
+  height: 450,
+  overflowX: "hidden",
+  overflowY: "scroll",
+  marginTop: 15,
+});
 export const artistIntro = style({
   width: "100%",
   height: 150,
@@ -80,65 +38,64 @@ export const artistIntroTitle = style({
   marginLeft: 15,
   fontSize: 20,
   color: "white",
-  fontFamily: "PyeongChangPeace",
+  fontFamily: `${vars.font.pyeongChangBold}`,
 });
 export const artistIntroContent = style({
   margin: 20,
   fontSize: 15,
   fontWeight: 400,
   color: "white",
-  fontFamily: "pretendard",
+  fontFamily: `${vars.font.pretendardRegular}`,
 });
 export const artistSongsContainer = style({
   width: "100%",
-  height: 300,
-  overflowY:"scroll",
+  height: "100%",
 });
 
 export const artistSongsList = style({
-    width: "100%",
-    height: 320,
-    
-  });
+  width: "100%",
+  height: 320,
+});
 
 export const artistSong = style({
-    width: "100%",
-    height: 80,
-    display:'flex',
-    alignItems:'center',
-    padding:"5px 10px"
-  });
+  width: "100%",
+  height: 80,
+  display: "flex",
+  alignItems: "center",
+  padding: "5px 10px",
+});
 
+export const albumCover = style({
+  width: 70,
+  height: 70,
+  borderRadius: 10,
+  backgroundColor: "gray",
+});
 
-export const albumCover=style({
-  width:70,
-  height:70,
-  borderRadius:10,
-  backgroundColor:'gray',
-})
-
-export const albumContainer=style({
-  width:"75%",
-  height:70,
-  display:'flex',
-  alignItems:'center',
-  flexDirection:'column',
-  padding:2,
-  position:'relative',
-})
-export const songTitle=style({
-  position:'absolute',
-  fontWeight:500,
-  margin:0,
-  left:10,
-  top:10,
-  fontSize:20,
-  color:"white",
-})
-export const albumTitle=style({
-  position:'absolute',
-  left:10,
-  bottom:15,
-  margin:0,
-  color:"rgba(255,255,255,0.6)",
-})
+export const albumContainer = style({
+  width: "75%",
+  height: 70,
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "column",
+  padding: 2,
+  position: "relative",
+});
+export const songTitle = style({
+  fontFamily: `${vars.font.pretendardRegular}`,
+  position: "absolute",
+  fontWeight: 500,
+  margin: 0,
+  left: 10,
+  top: 10,
+  fontSize: 20,
+  color: "white",
+});
+export const albumTitle = style({
+  fontFamily: `${vars.font.pretendardRegular}`,
+  position: "absolute",
+  left: 10,
+  bottom: 15,
+  margin: 0,
+  color: "rgba(255,255,255,0.6)",
+});
