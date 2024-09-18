e4cbdad5ccbbb0b3d31c19f3d8cca1a5328144c0
flareseek <flareseek@gmail.com>
2024-09-18 22:54:08 +0900

    chore: 주석 정리

--- src/shared/types/asset_types.ts
+++ src/shared/types/asset_types.ts
@@ -96,12 +96,6 @@ export class Booth extends Asset {
       `../../assets/data/booth_image/${place === "함인섭" ? "haminseop" : "playground/" + (place === "대운중앙노랑" ? "middle_yellow_line" : place === "대운상단초록" ? "top_green_line" : "bottom_pink_line")}/${imageName ?? this.id}.jpg`,
       import.meta.url,
     ).href;
-    // new URL로 없는 주소 호출시 에러 감지가 안됨...
-    // localhost:3000/assets/data/undefined 이런식으로 주소 변환되고
-    // 심지어 fetch로 호출해도 response ok로 뜸
-    // 그래서 undeifined로 끝날시 invalid 이미지로 대체 하도록 함
     if (this.image.endsWith("undefined")) this.image = invalidImg;
-    // 동적 import 할 시에 초기 값이 null이 될때 화면을 뿌려버리면 값이 바뀌어도 렌더링이 안되기 때문에 계속 null 이미로 남겨있음
-    // 이를 해결하기 위해 상태관리 라이브러리를 써야되는데 이대로 썼습니다.
   }
 }
