5b1118a9c7ce37560c6af40d5221682ca5f9eb98
dandamdandam <glue0440@gmail.com>
2024-09-18 20:53:46 +0900

    feat: 지도 스켈레톤 완

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import MapImg from "./MapImg";
-import usePlaceSelect from "./PlaceSelect";
+
+import { PLACE, PLACE_INFO } from "./mapData";
+
 export default function Map() {
-  const { placeNum, PlaceSelect } = usePlaceSelect();
+  const [placeNum, setPlaceNum] = useState(0);
   return (
     <div>
-      <PlaceSelect />
+      <select defaultValue={0} onChange={(e) => setPlaceNum(Number(e.target.value))}>
+        {PLACE.map((p, i) => (
+          <option key={p} value={i}>
+            {p}
+          </option>
+        ))}
+      </select>
       <MapImg placeNum={placeNum} />
+      <div>
+        <button>
+          <span className="material-symbols-outlined">list</span>
+          리스트 전체 보기
+        </button>
+        <p>{PLACE_INFO[placeNum]}</p>
+      </div>
     </div>
   );
 }
