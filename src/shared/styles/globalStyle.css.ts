0924166cf1562f0d31451fd7bc9d7592cc725c19
Jeje <jjh4450jjh4450@gmail.com>
2024-09-19 16:02:31 +0900

    fix

--- src/shared/styles/globalStyle.css.ts
+++ src/shared/styles/globalStyle.css.ts
@@ -1,15 +1,13 @@
 import { globalStyle } from "@vanilla-extract/css";
+
+import backgroundImg from "../../assets/background.webp";
 import { vars } from "./vars.css";
 
 globalStyle("body", {
   position: "relative",
-  background: "linear-gradient(180deg, #4FD1C5 0%, #56C5CF 16.67%, #5DB9D9 33.33%, #63ADE3 50%, #63A1ED 66.7%, #4FD1C5 100%)",
-  backgroundSize: "100% 2400px",
-  backgroundRepeat: "repeat-y",
-  minHeight: "100vh",
-  width: "100%",
+  backgroundImage: `url(${backgroundImg})`, // 배경 이미지 경로
+  backgroundSize: "100% auto",
 });
-
 globalStyle("#root", {
   fontFamily: vars.font.pretendardRegular,
 });
