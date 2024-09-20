9d8cc6b91b6b2e33f270631e062227b49e4a4d5f
Kim-jaeyeon <jy020507@naver.com>
2024-09-20 15:56:27 +0900

    Feat(artistData[id]로 접근 가능)
    useParams 사용 전, 일단 id 설정하면 접근 할 수 있도록 설정.

--- src/pages/Artist/index.tsx
+++ src/pages/Artist/index.tsx
@@ -15,56 +15,59 @@ import {
 } from "./artist.css.ts"; // 스타일 가져오기
 import { artistData } from "./artistData.ts";
 
+const id='Kwoneunbi';
+
+
 function Artist() {
   return (
     <div>
       <div className={mid}>
-        <img src={artistData.Choiyuree.picture} className={artistPicContainer}></img>
+        <img src={artistData[id].picture} className={artistPicContainer}></img>
         <div className={artistDetailContainer}>
           <div className={artistIntro}>
             <p className={artistIntroTitle}>아티스트 소개</p>
             <p className={artistIntroContent}>
-              {artistData.Choiyuree.description}
+              {artistData[id].description}
             </p>
           </div>
 
           <div className={artistSongsContainer}>
             <p className={artistIntroTitle}>아티스트 주요 인기곡</p>
             <div className={artistSongsList}>
-              <a className={artistSong} href={artistData.Choiyuree.firstSong[2]}>
+              <a className={artistSong} href={artistData[id].firstSong[2]}>
 
                 <div className={albumCover}></div>
                 <div className={albumContainer}>
-                  <p className={songTitle}>{artistData.Choiyuree.firstSong[0]}</p>
-                  <p className={albumTitle}>{artistData.Choiyuree.firstSong[1]}</p>
+                  <p className={songTitle}>{artistData[id].firstSong[0]}</p>
+                  <p className={albumTitle}>{artistData[id].firstSong[1]}</p>
                 </div>
               
               </a>
               
-              <a className={artistSong} href={artistData.Choiyuree.secondSong[2]}>
+              <a className={artistSong} href={artistData[id].secondSong[2]}>
 
                 <div className={albumCover}></div>
                 <div className={albumContainer}>
-                  <p className={songTitle}>{artistData.Choiyuree.secondSong[0]}</p>
-                  <p className={albumTitle}>{artistData.Choiyuree.secondSong[1]}</p>
+                  <p className={songTitle}>{artistData[id].secondSong[0]}</p>
+                  <p className={albumTitle}>{artistData[id].secondSong[1]}</p>
                 </div>
               
               </a>
-              <a className={artistSong} href={artistData.Choiyuree.thirdSong[2]}>
+              <a className={artistSong} href={artistData[id].thirdSong[2]}>
 
                 <div className={albumCover}></div>
                 <div className={albumContainer}>
-                  <p className={songTitle}>{artistData.Choiyuree.thirdSong[0]}</p>
-                  <p className={albumTitle}>{artistData.Choiyuree.thirdSong[1]}</p>
+                  <p className={songTitle}>{artistData[id].thirdSong[0]}</p>
+                  <p className={albumTitle}>{artistData[id].thirdSong[1]}</p>
                 </div>
               
               </a>
-              <a className={artistSong} href={artistData.Choiyuree.fourthSong[2]}>
+              <a className={artistSong} href={artistData[id].fourthSong[2]}>
 
                 <div className={albumCover}></div>
                 <div className={albumContainer}>
-                  <p className={songTitle}>{artistData.Choiyuree.fourthSong[0]}</p>
-                  <p className={albumTitle}>{artistData.Choiyuree.fourthSong[1]}</p>
+                  <p className={songTitle}>{artistData[id].fourthSong[0]}</p>
+                  <p className={albumTitle}>{artistData[id].fourthSong[1]}</p>
                 </div>
               
               </a>
