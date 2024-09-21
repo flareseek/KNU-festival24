128f3850bf3d3142cf7083937dbfceea74fdc5d3
Jeje <jjh4450jjh4450@gmail.com>
2024-09-22 00:37:03 +0900

    fix(BoothNFoodList\Card): 글자 shrink 설정 완료

--- src/pages/BoothNFoodList/Card/index.tsx
+++ src/pages/BoothNFoodList/Card/index.tsx
@@ -1,5 +1,5 @@
+import React from 'react';
 import { useNavigate } from "react-router-dom";
-
 import * as styles from "./index.css.ts";
 
 interface Props {
@@ -13,44 +13,36 @@ interface Props {
 }
 
 export default function Card({ id, name, color, description, order, imgURL, hasDetail }: Props) {
-  const navigator = useNavigate();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (hasDetail) {
+      navigate(`/booth_foodtruck_list/booth/${id}`);
+    }
+  };
 
   return (
-    <div
-      {...(hasDetail && { onClick: () => navigator(`/booth_foodtruck_list/booth/${id}`) })}
-      className={styles.container}
-    >
-      {imgURL && <img src={imgURL} alt={`${name} 부스/푸드트럭 이미지`} className={styles.image} />}
-      <div className={styles.contentContainer}>
-        <div>
-          {/*내용을 담는 div*/}
-          <div>
-            <div className={styles.contentDiv13}>
-              <h3 className={styles.title}>{name}</h3>
-            </div>
-            <div className={styles.contentDiv2}>
-              <p className={styles.description}>{description}</p>
-            </div>
+      <div className={styles.container} onClick={handleClick} role={hasDetail ? "button" : undefined}>
+        {imgURL && (
+            <img src={imgURL} alt={`${name} 부스/푸드트럭 이미지`} className={styles.image} />
+        )}
+          <div className={styles.contentContainer}>
+              <div className={styles.textContainer}>
+                  <h3 className={styles.title} title={name}>{name}</h3>
+                  <p className={styles.description} title={description}>{description}</p>
+              </div>
           </div>
-
-          {/*관련 정보 div*/}
-          <div>
-            <span className={styles.order} style={{ background: color }}>
-              No. {order}
-            </span>
-            {hasDetail && (
-              <div className={styles.contentDiv13}>
-                <div />
-                <div className={styles.detailBtn}>
-                  <span className={`material-symbols-outlined ${styles.detailBtnIcon}`}>info</span>
-                  <span>더보기</span>
-                </div>
+        <div className={styles.rightContainer}>
+        <span className={styles.order} style={{ background: color }}>
+          No. {order}
+        </span>
+          {hasDetail && (
+              <div className={styles.detailBtn}>
+                <span className={`material-symbols-outlined ${styles.detailBtnIcon}`}>info</span>
+                <span>더보기</span>
               </div>
           )}
-          </div>
-
         </div>
       </div>
-    </div>
   );
 }
