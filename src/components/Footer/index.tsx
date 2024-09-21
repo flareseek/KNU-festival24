2bb12431a61f22207206195ad346df54731f1f65
Jeje <jjh4450jjh4450@gmail.com>
2024-09-21 19:50:11 +0900

    footer 완성

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+import FooterLogo from '../../assets/footer.svg?react';
+import * as styles from './.css.ts';
+import { Link } from "react-router-dom";
+
+export function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <Link to={"/makers"}>
+                <FooterLogo className={styles.footerLogo} />
+            </Link>
+            <p className={styles.footerText}>
+                COPRYRIGHT Ⓒ 2024 LIKELION KANGWON NATIONAL UNIV.
+                ALL RIGHTS RESERVED.
+            </p>
+        </footer>
+    );
+}
