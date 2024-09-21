3694d16ed18e2b11ceca38cffa2bd98c32391989
Jeje <jjh4450jjh4450@gmail.com>
2024-09-22 03:09:35 +0900

    fix: overscrollBehavior가 데스크탑에서는 좀더 자유롭도록 수정

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,32 +2,11 @@ import { Outlet } from "react-router-dom";
 import "../../shared/styles/globalStyle.css";
 import Header from "../Header";
 import { Footer } from "../Footer";
-import * as style from "./.css.ts";
-import GlowStar from "../../assets/glow_star.json";
 
-glowStarStyles = [
-    style.glowStarStyle1,
-    style.glowStarStyle2,
-    style.glowStarStyle3,
-    style.glowStarStyle4,
-    style.glowStarStyle5,
-    style.glowStarStyle6,
-    style.glowStarStyle7,
-    style.glowStarStyle8,
-    style.glowStarStyle9,
-]
+// TODO: 모든 페이지에 공통으로 들어가는 레이아웃을 작성합니다. 필요 없으면 삭제해주세요
 export default function Layout() {
   return (
     <div>
-        glowStarStyles.map((style, index) => (
-            <Lottie
-                key={index}
-                className={style}
-                animationData={GlowStar}
-                loop
-                autoplay
-            />
-        ))
       <Header />
       <Outlet />
       <Footer />
