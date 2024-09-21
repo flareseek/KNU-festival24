a7f4ba680ab1d0133e589e03daa7842a8bd21b7d
dandamdandam <glue0440@gmail.com>
2024-09-21 10:50:45 +0900

    feat: 쿼리 파라미터 바꿔야 하는 탭 그거

--- src/pages/Map/MapImg/index.tsx
+++ src/pages/Map/MapImg/index.tsx
@@ -1,7 +1,7 @@
-import ham_square_island from "/src/assets/ham_square_island.png";
-import memorial_island from "/src/assets/memorial_island.png";
-import mirea_square_island from "/src/assets/mirea_square_island.png";
-import stadium_island from "/src/assets/stadium_island.png";
+import ham_square_island from "/src/assets/island/ham_square_island.png";
+import memorial_island from "/src/assets/island/memorial_island.png";
+import mirea_square_island from "/src/assets/island/mirea_square_island.png";
+import stadium_island from "/src/assets/island/stadium_island.png";
 
 import { PLACE } from "../mapData";
 import * as styles from "./index.css.ts";
