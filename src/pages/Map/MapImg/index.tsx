44e34432a4a37c7afc275b872d94a205f52dcc53
dandamdandam <glue0440@gmail.com>
2024-09-21 11:08:37 +0900

    feat: 탭별로 지도 변경

--- src/pages/Map/MapImg/index.tsx
+++ src/pages/Map/MapImg/index.tsx
@@ -3,17 +3,34 @@ import memorial_island from "/src/assets/island/memorial_island.png";
 import mirea_square_island from "/src/assets/island/mirea_square_island.png";
 import stadium_island from "/src/assets/island/stadium_island.png";
 
-import { PLACE } from "../mapData";
+import { Place } from "../mapData";
 import * as styles from "./index.css.ts";
 import { HamSquareObjs, MemorialObjs, MireaSquareObjs, StadiumObjs } from "./Objs";
 
-const islandList = [memorial_island, ham_square_island, stadium_island, mirea_square_island];
+const placeMap = {
+  "60주년기념관": {
+    img: memorial_island,
+    objs: <MemorialObjs />,
+  },
+  함인섭광장: {
+    img: ham_square_island,
+    objs: <HamSquareObjs />,
+  },
+  대운동장: {
+    img: stadium_island,
+    objs: <StadiumObjs />,
+  },
+  미래광장: {
+    img: mirea_square_island,
+    objs: <MireaSquareObjs />,
+  },
+} as const;
 
-export default function MapImg({ placeNum }: { placeNum: number }) {
+export default function MapImg({ place }: { place: string }) {
   return (
     <div className={styles.container}>
-      <img className={styles.mapImg} src={islandList[placeNum]} alt={`${PLACE[placeNum]} 지도`} />
-      <MireaSquareObjs />
+      <img className={styles.mapImg} src={placeMap[place as Place].img} alt={`${place} 지도`} />
+      {placeMap[place as Place].objs}
     </div>
   );
 }
--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -1,26 +1,26 @@
-import { useState } from "react";
 import MapImg from "./MapImg";
 
-import { PLACE, PLACE_INFO } from "./mapData";
+import { PLACE, PLACE_INFO, Place } from "./mapData";
+import { useTabs } from "../../shared/util/useTabs";
 
 export default function Map() {
-  const [placeNum, setPlaceNum] = useState(0);
+  const { activeTab, changeTab } = useTabs("place", [...PLACE]);
   return (
     <div>
-      <select defaultValue={0} onChange={(e) => setPlaceNum(Number(e.target.value))}>
-        {PLACE.map((p, i) => (
-          <option key={p} value={i}>
+      <select value={activeTab} onChange={(e) => changeTab(e.target.value)}>
+        {PLACE.map((p) => (
+          <option key={p} value={p}>
             {p}
           </option>
         ))}
       </select>
-      <MapImg placeNum={placeNum} />
+      <MapImg place={activeTab} />
       <div>
         <button>
           <span className="material-symbols-outlined">list</span>
           리스트 전체 보기
         </button>
-        <p>{PLACE_INFO[placeNum]}</p>
+        <p>{PLACE_INFO[activeTab as Place]}</p>
       </div>
     </div>
   );
