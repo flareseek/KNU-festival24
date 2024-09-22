40035623ee236d4f1e99abe5f828d9e30b54467a
Jeje <jjh4450jjh4450@gmail.com>
2024-09-22 14:46:27 +0900

    feat: 특정 페이지에 대해서 적용하기 쉽도록 변경

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,6 +3,7 @@ import "../../shared/styles/globalStyle.css";
 import Header from "../Header";
 import { Footer } from "../Footer";
 import { useEffect } from "react";
+import { useCurrentPage } from "../../shared/routing/routerUtils.ts";
 
 // TODO: 모든 페이지에 공통으로 들어가는 레이아웃을 작성합니다. 필요 없으면 삭제해주세요
 export default function Layout() {
@@ -17,11 +18,12 @@ export default function Layout() {
 }
 
 function ScrollToTop() {
-  const { pathname } = useLocation();
+  const currentPage = useCurrentPage();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    if (currentPage.scrollOptions === "never") return;
+    else window.scrollTo(0, 0);
+  }, [currentPage]);
 
   return null;
 }
