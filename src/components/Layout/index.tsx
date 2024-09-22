47f89f421a2ada7e5fa09899dea2e8a19be2064e
dandamdandam <glue0440@gmail.com>
2024-09-22 11:28:57 +0900

    fix: 페이지 이동 떄마다 스크롤 되게 수정

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,15 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import "../../shared/styles/globalStyle.css";
 import Header from "../Header";
 import { Footer } from "../Footer";
+import { useEffect } from "react";
 
 // TODO: 모든 페이지에 공통으로 들어가는 레이아웃을 작성합니다. 필요 없으면 삭제해주세요
 export default function Layout() {
   return (
     <div>
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
     </div>
   );
+}
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
 }
