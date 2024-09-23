4de155927e5fb3b64f2bbcccd012e1e12790c755
Jeje <jjh4450jjh4450@gmail.com>
2024-09-23 15:28:40 +0900

    fix: 한글날

--- src/shared/routing/routerInfo.ts
+++ src/shared/routing/routerInfo.ts
@@ -79,7 +79,7 @@ export const routerInfo: routerInfoType[] = [
     path: "timetable",
     element: lazy(() => import("../../pages/Timetable")),
     english: "Timetable",
-    korean: "실시간 일정",
+    korean: "축제 일정",
     expose: true,
     mainPage: true,
     scrollOptions: "never",
