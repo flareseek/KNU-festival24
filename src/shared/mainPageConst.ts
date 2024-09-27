8496ad23bdcda608841f031cbb3f0de4a381b32e
Jeje <jjh4450jjh4450@gmail.com>
2024-09-27 18:29:01 +0900

    라이선스 명시

--- src/shared/mainPageConst.ts
+++ src/shared/mainPageConst.ts
@@ -1,3 +1,14 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * All contributors who participated in development before September 26, 2024, retain full copyright over their contributions.
+ * These contributors are granted the right, under a Contributor License Agreement (CLA), to use, modify, and distribute their contributions
+ * under additional or alternative licensing terms of their choosing, while the project as a whole remains licensed under the MPL 2.0.
+ *
+ * Any contributions made after September 26, 2024, are subject to the terms of the MPL 2.0 and are licensed accordingly.
+ */
+
 /**
  * 메인페이지에 아직 라우터가 잡히지 않은 페이지들의 경로를 미리 지정해놓은 파일입니다.
  * 해당 페이지 제작후 아래 상수들을 사용하시면 됩니다.
