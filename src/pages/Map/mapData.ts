9013d63ba2ad8228ac0c8b0679c73729c7318e0a
Jeje <jjh4450jjh4450@gmail.com>
2024-09-27 18:42:01 +0900

    Revert "라이선스 명시"
    This reverts commit 8496ad23bdcda608841f031cbb3f0de4a381b32e.

--- src/pages/Map/mapData.ts
+++ src/pages/Map/mapData.ts
@@ -1,14 +1,3 @@
-/*
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
- * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
- *
- * All contributors who participated in development before September 26, 2024, retain full copyright over their contributions.
- * These contributors are granted the right, under a Contributor License Agreement (CLA), to use, modify, and distribute their contributions
- * under additional or alternative licensing terms of their choosing, while the project as a whole remains licensed under the MPL 2.0.
- *
- * Any contributions made after September 26, 2024, are subject to the terms of the MPL 2.0 and are licensed accordingly.
- */
-
 export const PLACE = ["60주년기념관", "함인섭광장", "대운동장", "미래광장"] as const;
 export type Place = (typeof PLACE)[number];
 
--- src/pages/Timetable/timeTableInfo.ts
+++ src/pages/Timetable/timeTableInfo.ts
@@ -1,14 +1,3 @@
-/*
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
- * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
- *
- * All contributors who participated in development before September 26, 2024, retain full copyright over their contributions.
- * These contributors are granted the right, under a Contributor License Agreement (CLA), to use, modify, and distribute their contributions
- * under additional or alternative licensing terms of their choosing, while the project as a whole remains licensed under the MPL 2.0.
- *
- * Any contributions made after September 26, 2024, are subject to the terms of the MPL 2.0 and are licensed accordingly.
- */
-
 import _apink from "../../assets/artist/에이핑크.jpg";
 import _choi_yu_ree from "../../assets/artist/최유리.jpg";
 import _creespy from "../../assets/artist/크리스피.jpg";
